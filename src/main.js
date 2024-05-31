7fe8b93d7e54724121b3c52e64d43a8d689f3eaa
Kateryna Sokur <k.o.sokur@gmail.com>
2024-05-31 20:36:51 +0200

    fixed

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
+
   const burger = document.querySelector('.burger-btn__open');
   const menu = document.querySelector('.menu');
   const closeMenu = document.querySelector('.burger-btn__close');
+  const body = document.body; 
   
   burger.addEventListener('click', () => {
   menu.classList.add('active');
+  body.style.overflow = 'hidden'; 
   });
   
   closeMenu.addEventListener('click', () => {
   menu.classList.remove('active');
+  body.style.overflow = ''; 
   });
 });
