f61048f9eca6631e6c5ae274f3822797e56a88a1
Kateryna Sokur <k.o.sokur@gmail.com>
2025-05-20 11:25:04 +0200

    added

---
+++ action_page.php
@@ -0,0 +1,22 @@
+<?php
+
+$name = $email = $phone = $successMessage = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+ $name = test_input($_POST["name"]);
+ $email = test_input($_POST["email"]);
+ $website = test_input($_POST["phone"]);
+
+
+}
+
+
+
+function test_input($data) {
+ $data = trim($data);
+ $data = stripslashes($data);
+ $data = htmlspecialchars($data);
+  return $data;
+}
+
+
